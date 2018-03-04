e6b72cbebd43de6243da17c69bcf0d00ddeed066
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-03-04 19:02:56 +0530

    Update letter2.java

--- BEGINNER/letter2.java
+++ BEGINNER/letter2.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
-class Num
+class Num2
 {
 	Scanner scan=new Scanner(System.in);
-	int n;
-	char letter;
+	int n1;
+	char letter1;
 	void getd()
 	{
-	     n=scan.nextInt();
+	     n1=scan.nextInt();
 		
-		letter=(char)(n+64);
-		System.out.print(letter);
+		letter1=(char)(n1+64);
+		System.out.print(letter1);
 		
 		    
 		    
@@ -22,8 +22,8 @@ public class numchar
 	
 	public static void main(String[] args)
 	{
-		Num p=new Num();
-		p.getd();
+		Num2 p1=new Num2();
+		p1.getd();
 
 	}
 
