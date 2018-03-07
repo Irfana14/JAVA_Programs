43c57b12f983ab96bad2e46dd6c26c32a57f5f6c
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-03-07 23:49:50 +0530

    Create ntimes.java

---
+++ BEGINNER/ntimes.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class T
+{
+	void getd()
+		
+	{
+		int n;
+		Scanner scan=new Scanner(System.in);
+		String s=scan.nextLine();
+		n=scan.nextInt();
+		for(int i=0;i<n;i++)
+			System.out.print(s+"\n");
+		
+		
+		
+	}
+}
+public class three {
+
+	
+	public static void main(String[] args) 
+	{
+	T p=new T();
+	p.getd();
+	}
+
+}
