09f987886e8581d2c90b35d4359e980158de6526
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-03-08 09:30:02 +0530

    Create area2.java

---
+++ BEGINNER/area2/area2.java
@@ -0,0 +1,27 @@
+package guvi;
+import java.util.*;
+class A
+{
+	Scanner scan=new Scanner(System.in);
+	int n,n1;
+	int Area;
+
+	void getd()
+	{
+		n=scan.nextInt();n1=scan.nextInt();
+		Area=(int) (((1)*(n)*(n1)))/2;
+		System.out.print(Area);
+	}//getd
+}
+
+public class matrixpro
+{
+   public static void main(String args[])
+  
+   {
+	  A p=new A();
+	  p.getd();
+	  
+   }
+   
+}
