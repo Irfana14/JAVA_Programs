14616835845fe382e43515ca21ec7699b2fe2823
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-03-08 22:31:18 +0530

    Create printwithoutsemi.java

---
+++ BEGINNER/area2/printwithoutsemi.java
@@ -0,0 +1,25 @@
+package MORESIMPLe;
+import java.util.Scanner;
+
+public class withoutsemi
+{
+
+	
+	public static void main(String[] args)
+	{
+		int i;
+		for(i=0;i<1;System.out.print("hello world"))
+		   i++;
+		
+	
+			
+		
+		
+		
+			
+			
+			
+
+	}
+
+}
