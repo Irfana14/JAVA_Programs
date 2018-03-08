4b351dc1e52292da79906f509a5305254e0bff5f
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-03-08 22:34:03 +0530

    Create multiple7.java

---
+++ BEGINNER/area2/multiple7.java
@@ -0,0 +1,24 @@
+package MORESIMPLe;
+
+import java.util.Scanner;
+
+public class firstnchar
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner scan=new Scanner(System.in); 
+		int s;
+		 char a[];
+		 s=scan.nextInt();
+		
+		 if(s%7==0)
+			 
+				System.out.print("yes"); 
+		 else
+			    System.out.print("no"); 
+			 
+
+	}
+
+}
