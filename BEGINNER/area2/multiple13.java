d35e1ce9a710b568c44e9042dd5a6a68f179a318
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-03-08 21:34:28 +0530

    Create multiple13.java

---
+++ BEGINNER/area2/multiple13.java
@@ -0,0 +1,32 @@
+package MORESIMPLe;
+
+import java.util.Scanner;
+
+public class multiple13 
+{
+
+	
+	public static void main(String[] args)
+	{
+		
+		   Scanner scan=new Scanner(System.in); 
+			int s;
+			 char a[];
+			 s=scan.nextInt();
+			
+			 if(s%13==0)
+				 
+					System.out.print("yes"); 
+			 else
+				    System.out.print("no"); 
+				 
+			 
+					 
+						 
+			 
+			
+		
+
+	}
+
+}
