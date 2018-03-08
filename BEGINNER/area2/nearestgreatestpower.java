ed598f6da8afdf0bc5fa96710391ba4ce64a6021
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-03-08 22:02:29 +0530

    Create nearestgreatestpower.java

---
+++ BEGINNER/area2/nearestgreatestpower.java
@@ -0,0 +1,19 @@
+package SIMPLE;
+import java.util.Scanner;
+
+public class nearestgreatestpower 
+{
+
+	
+	public static void main(String[] args) 
+	{
+		int n;
+		Scanner scan=new Scanner(System.in);
+		n=scan.nextInt();
+		if(n%2==0)
+			System.out.print(n*2);
+		
+
+	}
+
+}
