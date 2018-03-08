d2bbae24dcea6ac0cf0cbc1e2cfbc6c5b17adc5a
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-03-08 21:07:48 +0530

    Create round2.java

---
+++ BEGINNER/area2/round2.java
@@ -0,0 +1,24 @@
+package MORESIMPLe;
+import java.util.*;
+
+public class decimalroundoff 
+{
+	
+	
+	public static void main(String[] args)
+	{
+		 Scanner scan=new Scanner(System.in);
+		 String s;
+		 char a[];
+		 s=scan.nextLine();
+		 a=s.toCharArray();
+		
+		
+			System.out.print(Character.getNumericValue(a[0])+1);
+		
+		
+		
+
+	}
+
+}
