024192702dbd8e3320a944c553865c4dc3349803
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-03-08 19:48:47 +0530

    Create range.java

---
+++ BEGINNER/area2/range.java
@@ -0,0 +1,22 @@
+package MORESIMPLe;
+
+import java.util.Scanner;
+
+public class range 
+{
+
+	
+	public static void main(String[] args) 
+	{
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int n1=scan.nextInt();int n2=scan.nextInt();
+		if(n>n1&&n<n2)
+			System.out.print("yes");
+		else
+			System.out.print("no");
+		
+		
+	}
+
+}
