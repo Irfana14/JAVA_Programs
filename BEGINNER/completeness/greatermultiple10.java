81db61142c76c1c23370834fdb8abd22745942a7
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-03-09 07:09:27 +0530

    Create greatermultiple10.java

---
+++ BEGINNER/completeness/greatermultiple10.java
@@ -0,0 +1,21 @@
+package CODEKATTAA;
+
+import java.util.Scanner;
+
+public class nearestgreater10
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		for(int i=n;;i++)
+		{
+			if(i%10==0)
+				{System.out.print(i);
+			    break;}
+		}
+
+	}
+
+}
