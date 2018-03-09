bfec0649157706692d29a9e9f9d825211a7ab4eb
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-03-09 06:59:16 +0530

    Create count.java

---
+++ BEGINNER/completeness/count.java
@@ -0,0 +1,19 @@
+package CODEKATTAA;
+
+import java.util.Scanner;
+
+public class count1
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner scan=new Scanner(System.in);
+		String a2[]=scan.nextLine().split(" ");
+		System.out.print(a2.length);
+		
+		
+		
+		
+	}
+
+}
