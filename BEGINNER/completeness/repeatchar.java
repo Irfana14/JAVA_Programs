e60aea74d52ecd446da61b2a132b313bc5f92f89
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-03-21 19:30:34 +0530

    Update repeatchar.java

--- BEGINNER/completeness/repeatchar.java
+++ BEGINNER/completeness/repeatchar.java
@@ -1,54 +1,46 @@
-package geeks;
-import  java.io.*;
+package CODEKATTAA;
 import java.util.Scanner;
-class z
+class repeat
 {
-	Scanner scan=new Scanner(System.in);
-	int count=0;
+	String str;
 	void getd()
 	{
-		String s=scan.nextLine().toLowerCase();
 		
-		char[]a=s.toCharArray();
-		int n=s.length();
-		for(int i=0;i<(n-1);i++)
-		{
-			for(int j=(i+1);j<n;j++)
-			 {  
-				 if(a[i]==a[j])
-			    {
-				   a[j]='*';
-			    }//if
-			 }//for
-		}//for
-	for(int i=0;i<n;i++)
-		System.out.print(a[i]);
-	
-	for(int i=0;i<n;i++)
+		Scanner scan=new Scanner(System.in);
+		str=scan.nextLine();
+		char a[]=str.toCharArray();
 		
-		if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u')
 		
-		    count=count+1;
-	
-        System.out.print("\n"+count);		
-	
+		for(int i=0;i<(a.length-1);i++)
+			{
+			if(Character.isLetter(a[i]))
+			{  for(int j=i+1;j<a.length;j++)
+			  {
+				  if(a[i]==a[j])
+					  a[j]='*';
+				  
+			  }
+			}//if
 			
-	
+			}//for
+		for(int i=0;i<a.length;i++)
+			{
+			if(a[i]!='*')
+				System.out.print(a[i]);
+			}
+				
 		
-	}
-
-}
-
 		
-public class VOWELCOUNT 
+	}//getd
+}//repeat
+public class repeatcharremove
 {
 
-	
 	public static void main(String[] args) 
 	{
-		
-  z p=new z();
-  p.getd();
+		repeat p=new repeat();
+		p.getd();
+
 	}
 
 }
