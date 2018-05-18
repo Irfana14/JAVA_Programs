b632b921aae66adf0da8a41151b3480ed817f1e1
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-05-18 21:24:33 +0530

    Create hunter13.java

---
+++ CODEKATTA/HUNTER-2/hunter13.java
@@ -0,0 +1,30 @@
+package CODEKATTAA;
+import java.util.*;
+class P1
+{
+	Scanner scan=new Scanner(System.in);
+	String s;
+	void getd()
+	{
+	s=scan.nextLine();
+	StringBuffer sb=new StringBuffer(s);
+	String s1=sb.reverse().toString();
+	if(s1.equals(s))
+	
+		System.out.print("YES");
+	else
+		System.out.print("NO");
+	
+	}
+	
+}
+public class palindrome
+{
+
+	public static void main(String[] args)
+	{
+		P1 p=new P1();
+		p.getd();
+	}
+
+}
