23f142413370badcd3029d9710f4f6379b258d2d
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-05-20 20:07:36 +0530

    Create hunter16.java

---
+++ CODEKATTA/HUNTER-2/hunter16.java
@@ -0,0 +1,39 @@
+package JUNE;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+public class hunter16
+{
+
+	public static void main(String[] args)
+	{
+		Scanner scan=new Scanner(System.in);
+		int a[];
+		int n=scan.nextInt();int k=scan.nextInt();
+		a=new int[n];
+		ArrayList al=new ArrayList();
+		int count=0;
+		for(int i=0;i<n;i++)
+			a[i]=scan.nextInt();
+		for(int i=0;i<n;i++)
+			{
+			if(a[i]<k)
+			{
+				System.out.print(a[i]);
+				count=count+1;
+			}
+			if(a[i]>k)
+				al.add(a[i]);
+			}
+		Collections.sort(al);
+		for(int i=0;i<(3-(count));i++)
+			
+			System.out.print(al.get(i));
+				
+			
+			
+			
+
+	}
+
+}
