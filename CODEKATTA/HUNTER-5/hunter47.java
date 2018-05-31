1293d891a84d9c4cbf9deda88092bb13c31bcfe5
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-05-31 19:34:38 +0530

    Create hunter47.java

---
+++ CODEKATTA/HUNTER-5/hunter47.java
@@ -0,0 +1,19 @@
+package JUNE;
+
+import java.util.Scanner;
+
+public class hunter47 
+{
+	public static void main(String[] args)
+	{
+		Scanner scan=new Scanner(System.in);
+		String s=scan.nextLine();
+		System.out.print(s.replaceAll("( )+"," "));
+			
+		
+		
+		
+
+	}
+
+}
