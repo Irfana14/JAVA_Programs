e1bdcb7b2b543dad31c5cf23a8a8fc034853b6d3
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2018-06-02 14:37:21 +0530

    Create hunter51.java

---
+++ CODEKATTA/HUNTER-6/hunter51.java
@@ -0,0 +1,23 @@
+package JUNE;
+
+import java.util.Scanner;
+
+public class hunter51
+{
+
+public static void main(String[] args) 
+    {
+	String s1,s2;
+	
+	
+        Scanner scan=new Scanner(System.in);
+        s1=scan.nextLine();s2=scan.nextLine();
+        System.out.print(Integer.parseInt(s1)*Integer.parseInt(s2));
+        
+        
+        
+        
+
+	}
+
+}
